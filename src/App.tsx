63e679c5a4fcb0d5e5991e7e496bd4beaa7cb20d
pedromst2000 <40210465@esmad.ipp.pt>
2026-01-29 16:52:59 +0000

    Refactor: Simplify App component and update content for Tailwind v4 theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 
-function App() {
-	const [count, setCount] = useState(0);
+const App: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <p className="text- text-4xl font-family-piazzola-medium">
+        Albert Einstein 🚀
+      </p>
 
-	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
-	);
-}
+      <p className="text- text-4xl font-family-smoochSans-regular">
+        Tailwind v4 Custom Theme Working! 🚀
+      </p>
+    </div>
+  );
+};
 
 export default App;
