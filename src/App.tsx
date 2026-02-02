e74f9ff3f5bd72162861facb81c5c78efab91485
pedromst2000 <40210465@esmad.ipp.pt>
2026-02-02 18:44:59 +0000

    fix(fonts): correct Piazzolla spelling and update font paths

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from "react";
 
 const App: React.FC = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <p className="text- text-4xl font-family-piazzola-medium">
-        Albert Einstein 🚀
-      </p>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-(--bg-img) p-4">
+      <h1 className="font-piazzolla-regular text-[67px] text-primary  tracking-[0.05em] leading-tight">
+        Albert Einstein
+      </h1>
 
-      <p className="text- text-4xl font-family-smoochSans-regular">
-        Tailwind v4 Custom Theme Working! 🚀
+      <p className="font-smoochSans-regular text-[38px] text-primary tracking-[0.08em] mt-4 max-w-3xl text-center leading-snug">
+        "Life is like riding a bicycle. To keep your balance you must keep
+        moving."
       </p>
     </div>
   );
